0f6826840126300e13f9a1a326b72c7586bca37a
Jarrison Garcia <garciajarrison@gmail.com>
2018-12-26 11:30:25 -0500

    anexo de angular

---
+++ tiendaVirtualView/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CategoriaComponent } from './categoria/categoria.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CategoriaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
