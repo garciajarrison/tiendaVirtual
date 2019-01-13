dedeb293568f66e0a6c539d690f4ae4e8194bc64
Jarrison Garcia <garciajarrison@gmail.com>
2019-01-13 14:00:57 -0500

    ajustes finales

--- tiendaVirtualService/src/main/java/com/tiendavirtual/business/ICategoriaRepository.java
+++ tiendaVirtualService/src/main/java/com/tiendavirtual/business/ICategoriaRepository.java
@@ -1,16 +1,12 @@
 package com.tiendavirtual.business;
 
-import java.util.List;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.tiendavirtual.model.Categoria;
 
-@RepositoryRestResource(collectionResourceRel = "categoria", path = "categoria")
-public interface ICategoriaRepository extends PagingAndSortingRepository<Categoria, Integer> {
+@Repository
+public interface ICategoriaRepository extends JpaRepository<Categoria, Integer> {
 
-	List<Categoria> findByLastName(@Param("name") String name);
 
 }
