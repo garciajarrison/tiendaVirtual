0e9c7709c97669b2375754a067d32721f53731e3
kmilo desarrollo <mckamilo@gmail.com>
2019-01-23 15:20:17 -0500

    cambios en al empresa

--- tiendaVirtualService/src/main/java/com/tiendavirtual/model/Mensaje.java
+++ tiendaVirtualService/src/main/java/com/tiendavirtual/model/Mensaje.java
@@ -1,5 +1,43 @@
 package com.tiendavirtual.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.tiendavirtual.model.Mensaje.MensajesBuilder;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class Mensaje {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name = "mensaje")
+	private String mensaje;
+	
+	@Column(name = "valoracion")
+	private Integer valoracion;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="id_usuario")
+	private Usuario usuario;
+	
 
 }
